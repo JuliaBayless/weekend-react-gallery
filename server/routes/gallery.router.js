45e0eb4fc518e8ecbb67d9d3700277c5b7447662
Julia Bayless <julia.allistair@gmail.com>
2021-10-23 14:15:29 -0500

    server get working

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,23 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const pool = require ('../')
-const galleryItems = require('../modules/gallery.data');
+const pool = require('../modules/pool')
+// const galleryItems = require('../modules/gallery.data');
 
 
 // DO NOT MODIFY THIS FILE FOR BASE MODE
 
-
-// PUT Route
-router.put('/like/:id', (req, res) => {
-    console.log(req.params);
-    const galleryId = req.params.id;
-    for(const galleryItem of galleryItems) {
-        if(galleryItem.id == galleryId) {
-            galleryItem.likes += 1;
-        }
-    }
-    res.sendStatus(200);
-}); // END PUT Route
 
 // GET Route
 router.get('/', (req, res) => {
@@ -25,12 +13,28 @@ router.get('/', (req, res) => {
     SELECT * FROM "gallery";
     `;
     pool.query(sqlText)
-    .then((result) =>{
-        console.log('The GET is operational', result)
-        res.send(result.rows);
-    }).catch((error) =>{
-        console.log('error in the GET', error)
-    res.send(500);
-}); // END GET Route
+        .then((result) => {
+            console.log('The GET is operational', result)
+            res.send(result.rows);
+        }).catch((error) => {
+            console.log('error in the GET', error)
+            res.send(500);
+        }); // END GET Route
+});
+
+
+// PUT Route
+router.put('/like/:id', (req, res) => {
+    console.log(req.params);
+    const galleryId = req.params.id;
+    for (const galleryItem of galleryItems) {
+        if (galleryItem.id == galleryId) {
+            galleryItem.likes += 1;
+        }
+    }
+    res.sendStatus(200);
+}); // END PUT Route
+
+
 
 module.exports = router;
